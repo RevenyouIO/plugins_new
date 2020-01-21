5726a7d02bc9a23694ff3a3c5856562741e13860
Ryan Zander <rjameszander@gmail.com>
2020-01-21 19:13:12 +0700

    added import

--- packages/camera/android/src/main/java/io/flutter/plugins/camera/Camera.java
+++ packages/camera/android/src/main/java/io/flutter/plugins/camera/Camera.java
@@ -12,6 +12,7 @@ import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraCharacteristics.Key;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CameraMetadata;
